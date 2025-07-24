bc7e55e174dfb8dd615aab589e0b58493174898e
BenArbiaAhmed <hamoudabenarbia15@gmail.com>
2025-07-24 20:14:17 +0100

    dashboard page

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
+      <p className="text-[22px] md:text-[33px]">Wealth View</p>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[22px] md:text-[44px]">Wealth View</p>
     </div>
   );
 }
